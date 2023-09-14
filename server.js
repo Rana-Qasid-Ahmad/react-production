50a244b3bd54a82c396eb73f4022b706867677db
Rana <zrana1791@gmail.com>
2023-09-14 16:34:54 +0500

    ser

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
 const path = require('path');
 
-const app = express();
-const port = process.env.PORT || 3000; // Use the provided PORT environment variable or 3000 as a default
+const port = process.env.PORT || 3000;
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
+const server = http.createServer((req, res) => {
+  // Handle requests to serve the React build folder
+  if (req.url === '/') {
+    req.url = '/index.html'; // Serve the index.html file for the root URL
+  }
 
-// Serve the React app for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  const filePath = path.join(__dirname, 'build', req.url);
+  const contentType = getContentType(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // File not found, send a 404 response
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+      } else {
+        // Other error, send a 500 response
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      }
+    } else {
+      // File found, serve it with the appropriate content type
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.html':
+      return 'text/html';
+    case '.js':
+      return 'text/javascript';
+    case '.css':
+      return 'text/css';
+    case '.json':
+      return 'application/json';
+    case '.png':
+      return 'image/png';
+    case '.jpg':
+      return 'image/jpg';
+    default:
+      return 'application/octet-stream';
+  }
+}
